9dab89f9361c8fc866e266e61c0eb71f18899f5b
dimitriedmr <panainte.dimitrie@gmail.com>
2020-06-18 05:36:32 +0300

    update of UVa11029 test scripts and input file

--- UVa/UVa11029LeadingAndTrailing.py
+++ UVa/UVa11029LeadingAndTrailing.py
@@ -1,8 +1,5 @@
 import math
 
-base = 123456
-exponent = range(47,48)
-
 def binExp (b,e):
   ans = 1
   while(e > 0):
@@ -13,12 +10,22 @@ def binExp (b,e):
     print("new base: " + str(b) + " new answer: " + str(ans))
   return ans
 
-for ex in exponent:
-  power = pow(base,ex)
-  
-  stepPower = binExp(base,ex)
-  print(str(power) + " " + str(stepPower))
 
 if __name__ == '__main__':
+  stopCounter = 6
   with open("UVa11029LeadingAndTrailing.input") as f:
     content = f.readlines()
+  numbers = []   
+  print (content[0])
+  tests = int(content[0])
+  index = 1
+  while(tests):
+    b,e = [int(s) for s in content[index].split()]
+    index = index + 1
+    print(str(b) + "  " + str(e))
+    power = pow(b,e)
+    #stepPower = binExp(b,e)
+    print(str(power))# + " " + str(stepPower))
+    if(stopCounter < 1):
+      break;
+    stopCounter = stopCounter - 1;
